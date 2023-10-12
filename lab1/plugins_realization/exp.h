5194fb8ea00366021c9cbc2f8d4fcbfad9b94bff
eklipsss <lips2003@mail.ru>
2023-10-12 22:15:14 +0300

    delete

--- lab1/plugins_realization/exp.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <math.h>
-#include <string>
-#include <iostream>
-
-extern "C" __declspec(dllexport) std::string type();
-extern "C" __declspec(dllexport) bool binary();
-extern "C" __declspec(dllexport) int priority();
-extern "C" __declspec(dllexport) double operation(double x);
